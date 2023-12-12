345264ff543e5e1316a793c4f9072bc6f596e3ef
Danielin <danielxroh@gmail.com>
2023-12-12 16:04:59 +0100

    save navbar state on cookies

--- frontend/gympilot-next/src/components/navbar/Navbar.tsx
+++ frontend/gympilot-next/src/components/navbar/Navbar.tsx
@@ -6,27 +6,30 @@ import NavbarContent from './NavbarContent';
 import LogoutButton from './LogoutButton';
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const sidebarState = localStorage.getItem('sidebarOpen');
+  const [isOpen, setIsOpen] = useState(sidebarState === 'true');
 
-    const handleNavClick = () => {
-      setIsOpen(!isOpen);
-    }
-    const selectOption = () => {
-      setIsOpen(false);
-    }
-
-    return (
-      <>
-      <div className={`md:hidden ${!isOpen ? 'hidden' : ''} fixed z-10 bg-opacity-70 bg-black h-screen w-screen`}></div>
-      <nav className={`flex flex-col justify-between fixed md:relative h-full z-20 ${isOpen ? 'w-3/4 md:w-64 pl-6' : 'w-0'} bg-color-primary-strong text-color-font pt-6`}>
-        <NavbarContent isOpen={isOpen} selectOption={selectOption}/>
-        <NavbarButton handleClick={handleNavClick} isOpen={isOpen}/>
-        <div className={`flex items-center justify-start pb-5 ${!isOpen ? "hidden" : ""}`}>
-          <LogoutButton/>
-        </div>
-      </nav>
-      </>
-    );
+  const handleNavClick = () => {
+    const newState = !isOpen;
+    setIsOpen(newState);
+    localStorage.setItem('sidebarOpen', newState.toString());
   }
+  const selectOption = () => {
+    setIsOpen(false);
+  }
+
+  return (
+    <>
+    <div className={`md:hidden ${!isOpen ? 'hidden' : ''} fixed z-10 bg-opacity-70 bg-black h-screen w-screen`}></div>
+    <nav className={`flex flex-col justify-between fixed md:relative h-full z-20 ${isOpen ? 'w-3/4 md:w-64 pl-6' : 'w-0'} bg-color-primary-strong text-color-font pt-6`}>
+      <NavbarContent isOpen={isOpen} selectOption={selectOption}/>
+      <NavbarButton handleClick={handleNavClick} isOpen={isOpen}/>
+      <div className={`flex items-center justify-start pb-5 ${!isOpen ? "hidden" : ""}`}>
+        <LogoutButton/>
+      </div>
+    </nav>
+    </>
+  );
+}
   
   export default Navbar;
