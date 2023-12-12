c90dc8eda83ad25ef3beafe5057ce2e2c8e8fffc
Danielin <danielxroh@gmail.com>
2023-12-12 12:53:58 +0100

    implement timer

--- frontend/gympilot-next/src/components/createTrainings/createTrainingPhases/createSets/CreateSetTopper.tsx
+++ frontend/gympilot-next/src/components/createTrainings/createTrainingPhases/createSets/CreateSetTopper.tsx
@@ -1,7 +1,47 @@
-export default function CreateSetTopper() {
+import PauseIcon from "@/components/svg/PauseIcon";
+import PlayIcon from "@/components/svg/PlayIcon";
+import StopIcon from "@/components/svg/StopIcon";
+import { useState, useEffect } from "react";
+
+
+export default function CreateSetTopper({defaultTime2Wait = 95}:{defaultTime2Wait:number}) {
+  const [onPlay, setOnPlay] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(defaultTime2Wait);
+
+  function getMinutes() {
+    return Math.trunc(timeLeft / 60)
+  }
+  function getSeconds() {
+    return timeLeft % 60
+  }
+  const handlePlayPause = () => {
+    setOnPlay(!onPlay)
+  }
+  const handleStop = () => {
+    setOnPlay(false)
+    setTimeLeft(defaultTime2Wait)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!onPlay || timeLeft == 0) { return }
+      setTimeLeft(prevTime => prevTime - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [onPlay])
+
   return (
-    <div className="bg-color-info-back h-20 w-48 rounded-md -translate-y-2 p-4 flex items-center justify-center font-bold">
-      <span className="text-4xl">1:35</span>
+    <div className="bg-color-info-back h-20 w-48 rounded-md -translate-y-2 pt-4 flex flex-col items-center justify-center font-bold">
+      <span className="text-4xl">{getMinutes()}:{getSeconds()}</span>
+      <div className="flex items-center justify-center bg-color-primary-strong w-full rounded-b-md">
+        <button onClick={handlePlayPause}>
+          {!onPlay && <PlayIcon className="text-color-secondary h-8 w-8" />}
+          {onPlay && <PauseIcon className="text-color-secondary h-8 w-8" />}
+        </button>
+        <button onClick={handleStop}>
+          <StopIcon className="text-color-secondary h-8 w-8" />
+        </button>
+      </div>
     </div>
   );
 }
---
+++ frontend/gympilot-next/src/components/svg/StopIcon.tsx
@@ -0,0 +1,5 @@
+export default function KeyIcon({ className }: { className: string }) {
+  return (<svg className={className} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path d="M5.25 3A2.25 2.25 0 003 5.25v9.5A2.25 2.25 0 005.25 17h9.5A2.25 2.25 0 0017 14.75v-9.5A2.25 2.25 0 0014.75 3h-9.5z" />
+  </svg>);
+}
