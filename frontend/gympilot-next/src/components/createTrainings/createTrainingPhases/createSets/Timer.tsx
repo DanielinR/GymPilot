77c50ce94aa26210d1e8405a5ecb6d8eb022e748
Danielin <danielxroh@gmail.com>
2024-01-23 15:40:16 +0100

    fix timer when tab is not focused

--- frontend/gympilot-next/src/components/createTrainings/createTrainingPhases/createSets/Timer.tsx
+++ frontend/gympilot-next/src/components/createTrainings/createTrainingPhases/createSets/Timer.tsx
@@ -1,12 +1,14 @@
 import PauseIcon from "@/components/svg/PauseIcon";
 import PlayIcon from "@/components/svg/PlayIcon";
 import StopIcon from "@/components/svg/StopIcon";
+import { formatWithZeros } from "@/libs/utils";
 import { useState, useEffect } from "react";
 
 
 export default function Timer({defaultTime2Wait = 95}:{defaultTime2Wait?:number}) {
   const [onPlay, setOnPlay] = useState(false);
   const [timeLeft, setTimeLeft] = useState(defaultTime2Wait);
+  const [targetDate, setTargetDate] = useState(new Date(new Date().getTime() + (500 + defaultTime2Wait * 1000)));
 
   function getMinutes() {
     return Math.trunc(timeLeft / 60)
@@ -15,6 +17,7 @@ export default function Timer({defaultTime2Wait = 95}:{defaultTime2Wait?:number}
     return timeLeft % 60
   }
   const handlePlayPause = () => {
+    !onplay && setTargetDate(new Date(new Date().getTime() + (500 + defaultTime2Wait * 1000)))
     setOnPlay(!onPlay)
   }
   const handleStop = () => {
@@ -24,15 +27,16 @@ export default function Timer({defaultTime2Wait = 95}:{defaultTime2Wait?:number}
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (!onPlay || timeLeft == 0) { return }
-      setTimeLeft(prevTime => prevTime - 1);
-    }, 1000);
+      if (!onPlay || timeLeft <= 0) { return }
+      const diff = Math.floor((targetDate.getTime() - new Date().getTime()) / 1000);
+      setTimeLeft(diff);
+    }, 500);
     return () => clearInterval(interval);
-  }, [onPlay, timeLeft])
+  }, [onPlay, timeLeft, targetDate])
 
   return (
     <div className="bg-neutral-500 text-white bg-opacity-90 px-8 py-3 gap-1 shadow-xl rounded-t-md flex flex-col items-center justify-center font-bold">
-      <span className="text-4xl">{getMinutes()}:{getSeconds()}</span>
+      <span className="text-4xl">{formatWithZeros(getMinutes(),2)}:{formatWithZeros(getSeconds(),2)}</span>
       <div className="flex gap-2 items-center justify-center bg-color-primary-strong w-full rounded-b-md">
         <button className="bg-brand-500 rounded-full p-1" onClick={handlePlayPause}>
           {!onPlay && <PlayIcon className=" h-6 w-6" />}
