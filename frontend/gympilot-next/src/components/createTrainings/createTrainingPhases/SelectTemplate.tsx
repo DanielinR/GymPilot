34e617bb9fca57cfc0cd9d4d58c3ceddcf479408
Danielin <danielxroh@gmail.com>
2024-02-19 17:58:01 +0100

    fix routine not set on create training start

--- frontend/gympilot-next/src/components/createTrainings/createTrainingPhases/SelectTemplate.tsx
+++ frontend/gympilot-next/src/components/createTrainings/createTrainingPhases/SelectTemplate.tsx
@@ -3,12 +3,22 @@ import ListElement from "@/components/list/ListElementFunction";
 import { useContext } from "react";
 import { TrainingContext } from "../createTrainingContextProvider";
 import { Template, phases } from "@/libs/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function SelectTemplate() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   const { setPhase, setTemplate } = useContext(TrainingContext)!;
 
   const handleSelection = ({ id, name }: { id: number; name: string }) => {
     setTemplate({ id: id, name: name });
+    
+    const params = new URLSearchParams(searchParams);
+    params.set("routine", name);
+    router.replace(`${pathname}?${params.toString()}`);
+
     setPhase(phases.Exercises);
   };
   return (
